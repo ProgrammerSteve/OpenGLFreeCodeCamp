12d90c46c6f5b8936da441d2cd0ef2da8802b852
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-12-18 22:17:46 -0600

    Added a texture and stb library to handle images

--- main.cpp
+++ main.cpp
@@ -6,26 +6,47 @@
 #include "VBO.h"
 #include "EBO.h"
 #include "VAO.h"
+#include "stb/stb_image.h"
 
 //vertices coordinates for our triangle
 //XYZRGB
+//GLfloat vertices[] =
+//{
+//	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 0.8f, 0.3f, 0.02f,// Lower left corner
+//	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 0.8f, 0.3f, 0.2f,// Lower right corner
+//	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 1.0f, 0.6f, 0.32f,// Upper corner
+//	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, 0.9f,0.45f,0.17f,// Inner left
+//	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,0.9f,0.45f,0.17f, // Inner right
+//	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,0.8f,0.3f,0.2f // Inner down
+//};
+
+// Indices for vertices order
+//GLuint indices[] =
+//{
+//	0, 3, 5, // Lower left triangle
+//	3, 2, 4, // Upper triangle
+//	5, 4, 1 // Lower right triangle
+//};
+
+
+// Vertices coordinates
 GLfloat vertices[] =
-{
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 0.8f, 0.3f, 0.02f,// Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 0.8f, 0.3f, 0.2f,// Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 1.0f, 0.6f, 0.32f,// Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, 0.9f,0.45f,0.17f,// Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,0.9f,0.45f,0.17f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,0.8f,0.3f,0.2f // Inner down
+{ //     COORDINATES     /        COLORS       //
+	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
 };
+
+
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 3, 5, // Lower left triangle
-	3, 2, 4, // Upper triangle
-	5, 4, 1 // Lower right triangle
+	0, 2, 1, // Upper triangle
+	0, 3, 2 // Lower triangle
 };
+
 
 
 int main() 
@@ -62,22 +83,56 @@ int main()
 	EBO EBO1(indices, sizeof(indices));// Generates Element Buffer Object and links it to indices
 
 	// Links VBO to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6*sizeof(float),(void*)0);//coordinates
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3*sizeof(float)));//colors
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 *sizeof(float),(void*)0);//coordinates
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3*sizeof(float)));//colors
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));//textures
 
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
+	//can only be used after the shaderProgram is activated
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	//Texture image details, wxh number of color channels
+	int widthImg, heightImg, numColCh;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load("clock.png", &widthImg, &heightImg, &numColCh, 0);
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);//genertate mipmaps of other resolutions
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();// Tell OpenGL which Shader Program we want to use
+	glUniform1f(tex0Uni, 0);
 
 	while (!glfwWindowShouldClose(window)) 
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);// Specify the color of the background
 		glClear(GL_COLOR_BUFFER_BIT);// Clean the back buffer and assign the new color to it
 		shaderProgram.Activate();// Tell OpenGL which Shader Program we want to use
+
+		glUniform1f(uniID, 0.5f);
+		glBindTexture(GL_TEXTURE_2D, texture);
+
 		VAO1.Bind();// Bind the VAO so OpenGL knows to use it
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);// Draw primitives, number of indices, datatype of indices, index of indices
 		glfwSwapBuffers(window);// Swap the back buffer with the front buffer
 		glfwPollEvents();// Take care of all GLFW events
 	}
@@ -86,6 +141,7 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	glDeleteTextures(1,&texture);
 	shaderProgram.Delete();
 
 	glfwDestroyWindow(window);// Delete window before ending the program
