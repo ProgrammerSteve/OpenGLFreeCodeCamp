ba1fab1d9d0b1376ab34791bfb9e3f50aed721ab
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-12-20 21:06:17 -0600

    Used Model, view, projection matrices to change the display in 3D

--- main.cpp
+++ main.cpp
@@ -18,46 +18,30 @@
 
 
 
-//vertices coordinates for our triangle
-//XYZRGB
-//GLfloat vertices[] =
-//{
-//	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 0.8f, 0.3f, 0.02f,// Lower left corner
-//	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 0.8f, 0.3f, 0.2f,// Lower right corner
-//	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 1.0f, 0.6f, 0.32f,// Upper corner
-//	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, 0.9f,0.45f,0.17f,// Inner left
-//	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,0.9f,0.45f,0.17f, // Inner right
-//	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,0.8f,0.3f,0.2f // Inner down
-//};
-
-// Indices for vertices order
-//GLuint indices[] =
-//{
-//	0, 3, 5, // Lower left triangle
-//	3, 2, 4, // Upper triangle
-//	5, 4, 1 // Lower right triangle
-//};
-
-
 // Vertices coordinates
 GLfloat vertices[] =
 { //     COORDINATES     /        COLORS      /   TexCoord  //
-	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
-	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
-	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
-	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 };
-
-
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 2, 1, // Upper triangle
-	0, 3, 2 // Lower triangle
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
 };
 
 
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 int main() 
 {
@@ -68,7 +52,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "YoutubeOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -80,7 +64,7 @@ int main()
 	gladLoadGL();
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 	// Generates Shader object using shaders defualt.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
@@ -120,11 +104,23 @@ int main()
 		glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 proj = glm::mat4(1.0f);
+		view = glm::translate(view, glm::vec3(0.0f,-0.5f,-2.0f));
+		proj = glm::perspective(glm::radians(45.0f),(float)(width/height),0.1f,100.0f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
 
 		glUniform1f(uniID, 0.5f);
 		clock.Bind();
 		VAO1.Bind();// Bind the VAO so OpenGL knows to use it
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);// Draw primitives, number of indices, datatype of indices, index of indices
 		glfwSwapBuffers(window);// Swap the back buffer with the front buffer
 		glfwPollEvents();// Take care of all GLFW events
 	}
